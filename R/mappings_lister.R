e44c0965c03d0f7502625cdf48238cc6d6d5b283
AndreaCirilloAC <andreacirilloac@gmail.com>
2018-11-09 22:16:57 +0100

    fixed issue on mappings lister given from ~ character

--- R/mappings_lister.R
+++ R/mappings_lister.R
@@ -6,17 +6,21 @@
 mappings_lister <- function(plot_object,n_of_layers){
 
 
-  aes_db <-data.frame("aes" = plot_object$mapping %>% unlist() %>% names(),
-                                    "variable" = plot_object$mapping %>% unlist() %>% as.character(),stringsAsFactors = FALSE )
+  aes_db_raw <-data.frame("aes" = plot_object$mapping %>% unlist() %>% names(),
+                                     "variable" = plot_object$mapping %>% unlist() %>% as.character(),stringsAsFactors = FALSE )
 
   for(i in 1:n_of_layers){
     aes_names_partial <- plot_object$layers[[i]]$mapping %>% unlist() %>% names()
-    aes_values_partial <- plot_object$layers[[1]]$mapping %>% unlist() %>% as.character()
-    aes_db <- rbind(aes_db,data.frame("aes"=aes_names_partial,"variable"=aes_values_partial,stringsAsFactors = FALSE))
+    aes_values_partial_raw <- plot_object$layers[[1]]$mapping %>% unlist() %>% as.character()
+    #removing ~ character to ensure proper matching with variable names
+    aes_values_partial <- gsub("~","",x = aes_values_partial_raw)
 
-    return(aes_db)
+    aes_db_intermediate <- rbind(aes_db_raw,data.frame("aes" = aes_names_partial,"variable" = aes_values_partial,stringsAsFactors = FALSE))
+
   }
   #in the end I add the mapping set into ggplot()
+  aes_db_intermediate %>%
+    mutate(variable = gsub("~","",variable)) -> aes_db
+ return(aes_db)
 
-  return(aes_db)
 }
